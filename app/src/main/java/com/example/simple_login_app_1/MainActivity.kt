a1a5b8dc9b4f67df3ae5557da67946657d263e44
tkDev <153438920+tkDev0101@users.noreply.github.com>
2024-02-28 05:51:39 +0200

    UI Design
    Added background color and designed UI for login screen

--- app/src/main/java/com/example/simple_login_app_1/MainActivity.kt
+++ app/src/main/java/com/example/simple_login_app_1/MainActivity.kt
@@ -2,10 +2,34 @@ package com.example.simple_login_app_1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
+
+    //VARIABLES
+    lateinit var usernameInput : EditText
+    lateinit var passwordInput : EditText
+    lateinit var loginBtn : EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Assign Variables
+        usernameInput = findViewById(R.id.username_input)
+        passwordInput = findViewById(R.id.password_input)
+        loginBtn = findViewById(R.id.login_btn)
+
+        loginBtn.setOnClickListener()
+        {
+            val username = usernameInput.text.toString()
+            val password = passwordInput.text.toString()
+            Log.i("Test Credentials", "Username: $username and Password: $password")
+
+
+
+        }
+
     }
 }
